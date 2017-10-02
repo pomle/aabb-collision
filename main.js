f96b14763332127f99dc75d7f50e9feacacdcebd
Pontus Persson <pontus.alexander@gmail.com>
2017-10-02 19:36:29 +0200

    Add many followers.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import Rect from './Rect.js';
 import Vec from './Vec.js';
 
+function rand(max) {
+    return Math.random() * max;
+}
+
 const SPEED = 5;
 
 canvas.width = document.body.offsetWidth;
@@ -9,14 +13,24 @@ canvas.height = document.body.offsetHeight;
 const context = canvas.getContext('2d');
 const desired = new Vec(0, 0);
 
-const subject = new Rect(0, 0, 32, 32);
-const obstacles = new Array(30).fill(0).map(x => new Rect(
-    Math.random() * canvas.width,
-    Math.random() * canvas.height,
-    32 + Math.random() * 32,
-    32 + Math.random() * 32));
+const followers = new Array(10).fill(0).map(() => {
+    const s = 5 + rand(32);
+    const rect = new Rect(
+        rand(canvas.width),
+        rand(canvas.height),
+        s,
+        s);
+    rect.weight = rect.w * rect.h;
+    return rect;
+});
 
-function overlap(rect) {
+const obstacles = new Array(30).fill(0).map(() => new Rect(
+    rand(canvas.width),
+    rand(canvas.height),
+    32 + rand(32),
+    32 + rand(32)));
+
+function overlap(subject, rect) {
     return subject.b > rect.t
         && subject.t < rect.b
         && subject.r > rect.l
@@ -24,20 +38,21 @@ function overlap(rect) {
 }
 
 /* Iterate over all obstables that overlap subject. */
-function intersection(fn) {
-    obstacles.filter(overlap).forEach(fn);
+function intersection(subject, fn) {
+    obstacles.filter(obstacle => overlap(subject, obstacle)).forEach(fn);
 }
 
-function move(x, y) {
+function move(subject, x, y) {
     subject.x += x;
+
     if (x > 0) {
-        intersection(rect => {
+        intersection(subject, rect => {
             if (subject.r > rect.l) {
                 subject.r = rect.l;
             }
         });
     } else if (x < 0) {
-        intersection(rect => {
+        intersection(subject, rect => {
             if (subject.l < rect.r) {
                 subject.l = rect.r;
             }
@@ -46,13 +61,13 @@ function move(x, y) {
 
     subject.y += y;
     if (y > 0) {
-        intersection(rect => {
+        intersection(subject, rect => {
             if (subject.b > rect.t) {
                 subject.b = rect.t;
             }
         });
     } else if (y < 0) {
-        intersection(rect => {
+        intersection(subject, rect => {
             if (subject.t < rect.b) {
                 subject.t = rect.b;
             }
@@ -70,7 +85,9 @@ function drawRect(rect, color = '#000') {
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawRect(subject, 'red');
+    followers.forEach(subject => {
+        drawRect(subject, 'red');
+    });
 
     obstacles.forEach(rect => {
         drawRect(rect);
@@ -78,19 +95,21 @@ function draw() {
 }
 
 function update() {
-    const moveTo = new Vec(desired.x - subject.x, desired.y - subject.y);
-    if (moveTo.len > 5) {
-        moveTo.len /= 15;
-        move(moveTo.x, moveTo.y);
-    }
+    followers.forEach(subject => {
+        const moveTo = new Vec(desired.x - subject.x, desired.y - subject.y);
+        if (moveTo.len > 5) {
+            moveTo.len /= subject.weight * 0.2;
+            move(subject, moveTo.x, moveTo.y);
+        }
+    });
 
     draw();
     requestAnimationFrame(update);
 }
 
 canvas.addEventListener('mousemove', ({offsetX, offsetY}) => {
-    desired.x = offsetX - subject.w / 2;
-    desired.y = offsetY - subject.h / 2;
+    desired.x = offsetX;
+    desired.y = offsetY;
 });
 
 update();
