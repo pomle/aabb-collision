113657cb6fa0313f7d2ae50a465fca6b04c99ffb
Olle Sandberg <olle@b3rg.se>
2020-02-23 21:46:57 +0100

    Implement followers movement and collision detection

--- rust/src/rect.rs
+++ rust/src/rect.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Rect {
     pub x: f64,
     pub y: f64,
@@ -30,5 +30,21 @@ impl Rect {
 
     pub fn right(&self) -> f64 {
         self.x + self.width
+    }
+
+    pub fn set_right(&mut self, x: f64) {
+        self.x = x - self.width;
+    }
+
+    pub fn set_left(&mut self, x: f64) {
+        self.x = x;
+    }
+
+    pub fn set_top(&mut self, y: f64) {
+        self.y = y;
+    }
+
+    pub fn set_bottom(&mut self, y: f64) {
+        self.y = y - self.height;
     }
 }
